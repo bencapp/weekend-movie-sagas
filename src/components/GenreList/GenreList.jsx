10db01f875eccc810020e355bf1903a196717f41
Ben Capp <bentucker333@gmail.com>
2023-03-04 18:27:48 -0600

    created genrelist componenet

---
+++ src/components/GenreList/GenreList.jsx
@@ -0,0 +1,13 @@
+function GenreList() {
+  const genres = [1, 2, 3];
+  //
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default GenreList;
